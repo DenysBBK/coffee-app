e51c3241872911673cbd2987314773bbca5dc6c8
denys_b <denys.bbk@gmail.com>
2023-06-28 19:18:25 +0300

    Update main page

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -9,10 +9,10 @@
             <div class='sm:flex-row flex-col-reverse flex justify-between gap-x-5 gap-y-5'>
                 <p class='text-center bg-yellow-50 p-3 text-lg rounded-lg m-auto cover box-content inline-block border border-black'>
                     Would you like your coffee to be prepared as you walk to the coffee shop so that when you arrive, you can pick up your coffee and continue on your way?</p>
-                    <img src="../../images/queue.jpg" class='h-1/3 w-2/3 m-auto lg:h-2/3 lg:w-1/3'>
+                    <img src="../../images/making.jpg" class='h-1/3 w-2/3 m-auto lg:h-2/3 lg:w-1/3'>
             </div>
             <div class='sm:flex-row flex-col flex justify-between gap-x-5 gap-y-5'>
-                <img src="../../images/queue.jpg" class='h-1/3 w-2/3 m-auto lg:h-2/3 lg:w-1/3'>
+                <img src="../../images/order.jpg" class='h-1/3 w-2/3 m-auto lg:h-2/3 lg:w-1/3'>
                 <p class='text-left bg-yellow-50 p-3 text-lg rounded-lg m-auto cover box-content inline-block border border-black'>
                     All you need to do is 4 simple things:
                     <br><router-link to="/registration" class="text-blue-500">1. Register</router-link>
@@ -24,7 +24,7 @@
             <div class='sm:flex-row flex-col-reverse flex justify-between gap-x-5 gap-y-5'>
                 <p class='text-center bg-yellow-50 p-3 text-lg rounded-lg m-auto cover box-content inline-block border border-black'>
                     Enjoy your favorite coffee that you now have without queuing!</p>
-                    <img src="../../images/queue.jpg" class='h-1/3 w-2/3 m-auto lg:h-2/3 lg:w-1/3'>
+                    <img src="../../images/drink.jpg" class='h-1/3 w-2/3 m-auto lg:h-2/3 lg:w-1/3'>
             </div>
 
 
